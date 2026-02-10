13afcf3e704237446e34ecf19a6d6b38efd7ba0a
yexxjx <chenstyle02@gmail.com>
2026-02-10 13:15:40 +0900

    260210 점심코드

--- src/day12/Exam1.java
+++ src/day12/Exam1.java
@@ -2,6 +2,10 @@ package day12;
 
 import 종합.예제7.model.dto.BoardDto;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Exam1 {
     public static void main(String[] args) {
         /*
@@ -12,7 +16,7 @@ public class Exam1 {
         참조타입 : String, Scanner, MemberDto, [ ] 등
         - 기본 타입 외 모두 참조타입이다. 배열, 클래스, 인터페이스
         - 대문자로 시작함
-        - 기본 타입 외 개발자가 새로운 타입 만드는 행위, 예) int.double 2개 갖는 타입
+        - 기본 타입 외 개발자가 새로운 타입 만드는 행위, 예) int/double 2개 갖는 타입
          */
         // 자바 회사에서 제공하는 기본 클래스 등
         // [1] Object : 자바에서 최상위 클래스이면서 모든 클래스는 Object로 부터 상속받는다.
@@ -47,6 +51,56 @@ public class Exam1 {
         System.out.println(o10.hashCode()); // 1338668845
         System.out.println(o9.hashCode()); // 1338668845
         System.out.println(o8.hashCode()); // 159413332
+
+        // [2] Class : 클래스 정보(멤버변수/메소드 등등) 담는 클래스
+        String obj1=new String();
+        Class c1=obj1.getClass();
+        System.out.println(c1); // class java.lang.String
+
+        Integer obj2=3;
+        Class c2=obj2.getClass();
+        System.out.println(c2); // class java.lang.Integer
+
+        // 2-1) Class.forName("패키지명.클래스명");, 일반예외
+        // *** 리플랙션 *** : 최초 실행(컴파일) 할 때 객체를 생성하지 않음 실행 도중에 객체 생성=동적 처리
+        // 사용처 : JDBC(db연동), 스프링프레임워크(자바플랫폼) = 외부 라이브러리 동적 객체 생성
+        try{Class.forName("class java.lang.String");} // String 클래스가 존재하면 객체가 동적으로 생성한다
+        catch(ClassNotFoundException e){ }
+
+        // 2-2)
+        Field[] fields=c1.getFields(); // 객체 내 모든 멤버변수/속성/필드 명 확인(*private 제외)
+        for(int i=0; i<fields.length; i++){
+            System.out.println(fields[i]);
+        }
+
+        Constructor[] constructors=c1.getConstructors(); // 클래스 내 모든 생성자 확인(매개변수 확인)
+        for(int i=0; i<constructors.length; i++){
+            System.out.println(constructors[i]); // 15개, 오버로딩 이용한 다수의 생성자
+        }
+
+        Method[] methods=c1.getMethods(); // 클래스 내 모든 메소드 확인
+        for(int i=0; i< methods.length; i++){
+            System.out.println(methods[i]); // 모든 메소드 전부 나옴
+        }
+
+        // [3] 래퍼 클래스 : 기본 타입 > 참조타입으로 표현, 장점 : 기본 타입은 메소드가 없다
+        int value1=100; // 자료는 100, 분류/타입 int
+        Integer value2=100; // 자료는 100, 분류/타입 Integer
+        // System.out.println(value1.toString());
+        System.out.println(value2.toString()); // Integer 참조타입으로 기능(메소드)이 있음
+        // 3-1) 언박싱과 오토박싱
+        int value3=value2; // Integer > int 언박싱, 참조 타입 > 기본 타입
+        Integer value4=value1; // int > Integer 오토박싱, 기본 타입 > 참조 타입
+
+        // 자바에서 외부 자료들과 자료들을 주고 받을 때 타입 변환 (엑셀/CSV/공공데이터API/JS통신/PYTHON통신)
+        int val1=Integer.parseInt("100"); // 문자 "100"을 숫자 100으로 타입 변환
+        double val2=Double.parseDouble("3.14"); // 문자 "3.14"를 숫자 3.14로 타입 변환
+        float val3=Float.parseFloat("3.14"); // 문자 "3.14"를 숫자 3.14로 타입 변환
+        boolean val4=Boolean.parseBoolean("true"); // 문자 "true"를 true로 타입 변환
+
+        // 기본 타입 > 문자열 변환
+        String s1=100+""; // 기본타입자료+"" 100 > "100"
+        String s2=String.valueOf(100); // 100 > "100"
 
     }
 }
