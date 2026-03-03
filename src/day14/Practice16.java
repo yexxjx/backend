ac504ab79b46f881b430a338f8dd6f04219d5918
yexxjx <chenstyle02@gmail.com>
2026-03-03 21:53:32 +0900

    21:53

--- src/day14/Practice16.java
+++ src/day14/Practice16.java
@@ -1,35 +1,32 @@
 package day14;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Practice16 {
-    public static void main(String[] args) {
-        Runnable runnable=new Runnable() {
-            @Override
-            public void run() {
-                for(int i=1; i<=3; i++){
-                    try{Thread.sleep(1000);}
-                    catch (Exception e){}
-                }
-            }
-        };
 
-        Thread thread1=new Thread(runnable);
+    public static void main(String[] args) {
+        DeliveryTask task = new DeliveryTask();
+        Thread thread1 = new Thread(task);
         thread1.start();
-        for(int i=1; i<=3; i++){
-            try{Thread.sleep(1000);}
-            catch (Exception e){}
+
+        for(int i=1; i<=3; i++) {
+            System.out.println("[메인] 주문 화면 갱신 " + i);
+            try {Thread.sleep(1000);}
+            catch(Exception e) {}
         }
+
+        try {thread1.join();}
+        catch(Exception e) {}
+        System.out.println("[안내] 배달 처리 종료");
     }
 }
-class DeliveryTask implements Runnable{
+
+class DeliveryTask implements Runnable {
+
     @Override
     public void run() {
-        for( ; ; ){
-            System.out.println("[배달기사] 이동중");
-            try{Thread.sleep(1000);}
-            catch(Exception e){}
+        for(int i=1; i<=3; i++) {
+            System.out.println("[배달기사] 이동중 " + i);
+            try {Thread.sleep(1000);}
+            catch(Exception e) {}
         }
     }
 }
